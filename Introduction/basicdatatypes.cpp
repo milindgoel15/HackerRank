fc03c92a804d96445de11b57a08d20146a1c6fc4
Milind Goel <milindgoel15@gmail.com>
2021-11-22 09:43:41 +0530

    more

---
+++ Introduction/basicdatatypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a = 3;
+    long b = 12345678912345;
+    char c = 'a';
+    float d = 334.23;
+    double e = 14049.30493;
+    
+    printf("%d \n", a);
+    printf("%ld \n", b);
+    printf("%c \n", c);
+    printf("%f \n", d);
+    printf("%lf \n", e);
+    
+    return 0;
+}
