2013828562f87bf4c07dfdf48bc6895e105b7eb0
Milind Goel <milindgoel15@gmail.com>
2021-11-23 10:23:00 +0530

    fix

--- Introduction/basicdatatypes.cpp
+++ Introduction/basicdatatypes.cpp
@@ -4,17 +4,18 @@ using namespace std;
 
 int main() {
     // Complete the code.
-    int a = 3;
-    long b = 12345678912345;
-    char c = 'a';
-    float d = 334.23;
-    double e = 14049.30493;
-    
-    printf("%d \n", a);
-    printf("%ld \n", b);
-    printf("%c \n", c);
-    printf("%f \n", d);
-    printf("%lf \n", e);
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;    
+
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", c);
+    printf("%f\n", d);
+    printf("%lf\n", e);
     
     return 0;
 }
