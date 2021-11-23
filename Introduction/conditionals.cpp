13228c76175a5d9da563e82de859d14446a825a5
Milind Goel <milindgoel15@gmail.com>
2021-11-23 11:01:26 +0530

    exact output fix

--- Introduction/conditionals.cpp
+++ Introduction/conditionals.cpp
@@ -6,40 +6,39 @@ using namespace std;
 int main() {
     int n = 0;
     
-    cout <<"Enter your number: ";
     cin >> n;
     
     if (n == 0) {
-        cout << "\nThe english word for " << n << " is zero.";
+        cout << "zero";
     }
     else if (n == 1) {
-        cout << "\nThe english word for " << n << " is one.";
+        cout << "one";
     }
     else if (n == 2) {
-        cout << "\nThe english word for " << n << " is two.";
+        cout << "two";
     }
     else if (n == 3) {
-        cout << "\nThe english word for " << n << " is three.";
+        cout << "three";
     }
     else if (n == 4) {
-        cout << "\nThe english word for " << n << " is four.";
+        cout << "four";
     }
     else if (n == 5) {
-        cout << "\nThe english word for " << n << " is five.";
+        cout << "five";
     }
     else if (n == 6) {
-        cout << "\nThe english word for " << n << " is six.";
+        cout << "six";
     }
     else if (n == 7) {
-        cout << "\nThe english word for " << n << " is seven.";
+        cout << "seven";
     }
     else if (n == 8) {
-        cout << "\nThe english word for " << n << " is eight.";
+        cout << "eight";
     }
     else if (n == 9) {
-        cout << "\nThe english word for " << n << " is nine.";
+        cout << "nine";
     }
     else {
-        cout << "\nThe number is greater than 9.";
+        cout << "Greater than 9";
     }
 }
